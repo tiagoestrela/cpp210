fb68f4459a66a303962dca85c8e7a3271b7d52e0
tiagoestrela <56174000+tiagoestrela@users.noreply.github.com>
2020-08-13 20:06:02 -0300

    Add files via upload

---
+++ aula1T33.py
@@ -0,0 +1,45 @@
+#SAÍDA DE DADOS
+#A print() serve para exibir na tela para o usuário
+#informações como uma mensagem ou conteúdos
+#de uma variável
+
+#IMPRIMIR TEXTO->
+#conteúdo fixo
+#sintaxe print(" texto")
+#\n pula linha
+#\n dentro das aspas duplas da função print
+print("Quero ver o jogo do psg.  \n ")
+
+#IMPRIMIR VARIÁVEL
+#sintaxe print(nome_da_variável)
+nome="Tiago"
+idade=38
+print(nome)
+print(idade)
+# sintaxe print() serve para pular linha
+print()
+#IMPRIMIR TEXTO E VARIÁVEL
+#sintaxe print("texto",nome_da_variável)
+
+#ENTRADA DADOS
+#função que serve para usuário entrar com
+#com os dados via digitação
+#sintaxe->input("diga o que você quer colocar")
+
+nome=input("Diga seu nome: \n")
+#entrada de dados-> meu nome-> colocado caixinha nome
+print("Boa noite", nome, "até amanhã!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
