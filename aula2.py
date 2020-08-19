f76e3540d0ba39ddf77e823ae75bad189a252a2e
tiagoestrela <56174000+tiagoestrela@users.noreply.github.com>
2020-08-19 18:47:18 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,130 @@
+#sintaxe para entrada de dados
+#input("texto")
+nome=input("Insira seu nome: \n")
+idade=input("Insira sua idade: \n")
+#\n pula linha
+#tipo dado-> número inteiro-> int
+#sintaxe para saída de dados
+#print()
+#saída de texto
+#texto é um tipo de variável chamda de string-> str
+#Qualquer coisa que coloque entre aspas duplas é uma strig
+#sintaxe para para exibir texto é print("texto")
+print("Tiago sua idade é: \n")
+#sintaxe para exibir o conteúdo da variável é
+#print(nome_variável)
+print(idade)
+#sintaxe para exibir variável e texto é:
+print(nome,"sua idade é \n:")
+print(idade)
+# mesma coisa feita diferentemente
+print(nome,"sua idade é",idade,"anos.")
+#variável
+#nome
+#só pode inciar com letra
+#não pode ter espaço
+#exceto (_) sublinha
+#p1-> pode
+#prof fei->não pode
+#proffei->pode
+#aluno_fei->pode
+#_fei->pode
+#1p->não pode
+
+
+
+
+
+#1) numéricas-> inteiras ou decimais
+#type é tipo
+#inteiras->type int
+#decimais->type float
+#2) caracter->texto->string->type str
+# 3) booleanas->Verdadeira ou Falsa->type bool->True ou False
+#o T e o F são maiúsculos
+#a=2 é inteira
+#b=2.0 é decimal
+#c= True é bool
+#d="oi"
+#e="38"
+# para determinar o tipo da variável usamos a sehuinte sintaxe.
+#type(nome_variável)
+
+#operadores matemáticos
+#+,-,*,/
+#** potenciação
+# // divisão inteira
+# % resto da divisão inteira
+# 9 dividido por 2 dá 4 com resto 1
+#9=4*2+1
+#9div2=4
+#9//2=4
+#9mod2=1
+#9%2=1
+# ordem de prioridade
+#1ª) **
+# 2ª)  *, /,//,%
+#3ª) +,-
+
+
+
+
+
+
+
+
+
+#operadores relacionais
+#relacionam os número
+#>,<,>=,<=,==, !=
+
+# um igual só é atribuição que é diferente de dois iguais
+#== é comparação
+# é a igualdade matemática
+# != diferença
+
+
+# 9>2->True
+#3==2->False
+#3==3->True
+#3>=3-> True
+#5<8-> False
+#2 !=3 -> True
+
+#para pacotes já instalados
+#como biblioteca na math
+# devo fazer a importação toda vez que inicializar o python
+# import math
+#para usar as funções da biblioteca math
+#math.funcao
+# para usar o número pi usamos a sintaxe
+#math.pi
+#para evitar o usar o nome da biblioteca
+# from math import*
+# não preciso usar o math. basta digitar pi
+
+#para pacotes instalados
+#instalar o gerenciador de programas do python
+#pip
+#usar terminal-> prompt
+#instalar usando o pip o jupyter lab
+# digitar no promp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
